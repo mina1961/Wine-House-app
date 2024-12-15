374f3e7f2f550742b9e39049ac546304c3f8c939
mina bergova <bergovam@gmail.com>
2024-12-15 19:27:53 +0200

    initial project setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'wine-house';
+
+  constructor(private http: HttpClient) {};
+
+  ngOnInit() {}
+}
